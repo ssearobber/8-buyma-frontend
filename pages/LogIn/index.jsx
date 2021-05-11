d591e8df496f71e8477f8138f78c5a0561a04507
ssearobber <ssearobber@naver.com>
2021-05-11 17:28:58 +0900

    make it better

--- pages/LogIn/index.jsx
+++ pages/LogIn/index.jsx
@@ -62,7 +62,7 @@ const LogIn = () => {
       </Form>
       <LinkContainer>
         まだ会員ではないですか?&nbsp;
-        <a href="https://relaxed-northcutt-53ebbd.netlify.app/signup">会員登録しに行く。</a>
+        <a href="/signup">会員登録しに行く。</a>
       </LinkContainer>
     </div>
   );
