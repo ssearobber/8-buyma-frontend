f2af99b3fcb4584c62c2b33f19459b574849aa47
ssearobber <ssearobber@naver.com>
2021-05-18 22:18:37 +0900

    make it better

--- layouts/Product/index.jsx
+++ layouts/Product/index.jsx
@@ -78,6 +78,60 @@ const Product = () => {
   if (productError) return <div>failed to load</div>
   // if (product == undefined) return <div>loading...</div>
 
+  // const CommentList = ({ comments }) => (
+  // <List
+  //   dataSource={comments}
+  //   header={`${comments.length} ${comments.length > 1 ? 'replies' : 'reply'}`}
+  //   itemLayout="horizontal"
+  //   renderItem={props => <Comment {...props} />}
+  // />
+  // );
+
+  // const Editor = ({ onChange, onSubmit, submitting, value }) => (
+  // <>
+  //   <Form.Item>
+  //     <TextArea rows={4} onChange={onChange} value={value} />
+  //   </Form.Item>
+  //   <Form.Item>
+  //     <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
+  //       Add Comment
+  //     </Button>
+  //   </Form.Item>
+  // </>
+  // );
+
+  //   handleSubmit = () => {
+  //   if (!this.state.value) {
+  //     return;
+  //   }
+
+  //   this.setState({
+  //     submitting: true,
+  //   });
+
+  //   setTimeout(() => {
+  //     this.setState({
+  //       submitting: false,
+  //       value: '',
+  //       comments: [
+  //         ...this.state.comments,
+  //         {
+  //           author: 'Han Solo',
+  //           avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+  //           content: <p>{this.state.value}</p>,
+  //           datetime: moment().fromNow(),
+  //         },
+  //       ],
+  //     });
+  //   }, 1000);
+  // };
+
+  // handleChange = e => {
+  //   this.setState({
+  //     value: e.target.value,
+  //   });
+  // };
+
   return (
     <div>
       <Header>
@@ -137,6 +191,25 @@ const Product = () => {
                 
               </List>
             </CommentForm> */}
+            {/* <>
+            {comments.length > 0 && <CommentList comments={comments} />}
+            <Comment
+              avatar={
+                <Avatar
+                  src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                  alt="Han Solo"
+                />
+              }
+              content={
+                <Editor
+                  onChange={this.handleChange}
+                  onSubmit={this.handleSubmit}
+                  submitting={submitting}
+                  value={value}
+                />
+              }
+            />
+          </> */}
           </div>
         </Container>)}
       <ToastContainer t="bottom-center" />
